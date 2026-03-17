0a71aacc8cd0866688f879a7bdb8acd47f332d47
prince kaushik <princekaushik9926@gmail.com>
2026-03-17 11:26:56 +0530

    Update_build_script

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
-echo "Installing Frontend Dependencies..."
+echo "==> Installing Frontend Dependencies..."
 npm install --prefix frontend
 
-echo "Building Frontend..."
+echo "==> Building Frontend..."
 npm run build --prefix frontend
 
-echo "Installing Backend Dependencies..."
+echo "==> Installing Backend Dependencies..."
 pip install -r backend/requirements.txt
+
+echo "==> Build Complete!"
